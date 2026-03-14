166a1a79033714646a7e25a0ad0cfc94fb59892e
User <weixing.khant@gmail.com>
2026-03-14 18:46:46 +0700

    removed unused useEffect import

--- src/features/backend-explorer/hooks/useBackendExplorerController.ts
+++ src/features/backend-explorer/hooks/useBackendExplorerController.ts
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useMemo} from 'react';
+import {useCallback, useMemo} from 'react';
 import { getItemId, getRecipeTileId } from '../utils/ids';
 import useExplorerCreateFlow from './useExplorerCreateFlow';
 import useExplorerDeleteFlow from './useExplorerDeleteFlow';
