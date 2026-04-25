090b93c789b748d2776cc03f92e0180e80a7541d
khant-nyer <58938149+khant-nyer@users.noreply.github.com>
2026-04-25 16:47:58 +0700

    Use transparent icon assets and scale icons in dashboard badges

---
+++ src/components/iconAssets.ts
@@ -0,0 +1,7 @@
+export const iconAssets = {
+  menuChefHat: 'https://cdn-icons-png.flaticon.com/512/1830/1830767.png',
+  food: 'https://cdn-icons-png.flaticon.com/512/1667/16678071.png',
+  ingredient: 'https://cdn-icons-png.flaticon.com/512/1902/19020249.png',
+  recipe: 'https://cdn-icons-png.flaticon.com/512/1667/16678071.png',
+  nutrition: 'https://cdn-icons-png.flaticon.com/512/1161/11615122.png'
+} as const;
