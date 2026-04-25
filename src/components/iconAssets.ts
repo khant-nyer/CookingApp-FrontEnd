551d9d248ace1607abf0a37bbcdfc7e51dd7b977
khant-nyer <58938149+khant-nyer@users.noreply.github.com>
2026-04-25 19:06:19 +0700

    Update sidebar tab icons to requested Flaticon assets

--- src/components/iconAssets.ts
+++ src/components/iconAssets.ts
@@ -1,9 +1,9 @@
 export const iconAssets = {
   menuChefHat: 'https://cdn-icons-png.flaticon.com/512/1830/1830767.png',
-  food: 'https://cdn-icons-png.flaticon.com/512/1667/16678071.png',
-  ingredient: 'https://cdn-icons-png.flaticon.com/512/1902/19020249.png',
-  recipe: 'https://cdn-icons-png.flaticon.com/512/1830/1830767.png',
-  nutrition: 'https://cdn-icons-png.flaticon.com/512/1161/11615122.png',
+  food: 'https://cdn-icons-png.flaticon.com/512/1721/1721532.png',
+  ingredient: 'https://cdn-icons-png.flaticon.com/512/601/601990.png',
+  recipe: 'https://cdn-icons-png.flaticon.com/512/13525/13525873.png',
+  nutrition: 'https://cdn-icons-png.flaticon.com/512/13525/13525873.png',
   foodSummaryAnimated: 'https://cdn-icons-gif.flaticon.com/19018/19018393.gif',
   ingredientSummaryAnimated: 'https://cdn-icons-gif.flaticon.com/19020/19020222.gif',
   recipeSummaryAnimated: 'https://cdn-icons-gif.flaticon.com/16678/16678005.gif',
