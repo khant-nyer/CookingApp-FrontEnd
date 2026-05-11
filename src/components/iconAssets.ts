f52ef70fc9a7987a06ff12e48cf670c9f60a7fcb
khant-nyer <58938149+khant-nyer@users.noreply.github.com>
2026-05-11 14:24:32 +0700

    Make sidebar menu icon fully transparent and borderless

--- src/components/iconAssets.ts
+++ src/components/iconAssets.ts
@@ -1,5 +1,5 @@
 export const iconAssets = {
-  menuChefHat: 'https://cdn-icons-png.flaticon.com/512/1830/1830767.png',
+  menuChefHat: 'https://cdn-icons-png.flaticon.com/512/1830/1830839.png',
   food: 'https://cdn-icons-png.flaticon.com/512/5746/5746471.png',
   ingredient: 'https://cdn-icons-png.flaticon.com/512/601/601990.png',
   recipe: 'https://cdn-icons-png.flaticon.com/512/13525/13525873.png',
